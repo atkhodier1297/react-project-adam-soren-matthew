0ec75323ad06cea2513621eb87de00e41b5be471
Adam Khodier <khodieradam1297@gmail.com>
2022-10-25 12:41:33 -0400

    cards rendered

---
+++ src/components/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Recipe({recipe}) {
+    const {name, url, description, ingredients, time} = recipe
+  return (
+    <div class="card">
+  <div class="container">
+    <h4><b>{name}</b></h4>
+    <img src={url} alt={name}/>
+    <p>{description}</p>
+    <p>{ingredients}</p>
+    <p>{time}</p>
+
+  </div>
+</div>
+  )
+}
+
+export default Recipe
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,21 +1,13 @@
 import React from 'react'
+import Recipe from './Recipe'
 
 function RecipeList({recipes}) {
-  const {name, description, ingredients, url, time} = recipes
-  const eachRecipe = recipes.map(recipe => <RecipeList recipe={recipe} key={recipe.id}/>
+
+  const eachRecipe = recipes.map(recipe => <Recipe recipe={recipe} key={recipe.id}/>
   )
 
   return (
-    <li className="recipe card">
-    <div className="image">
-      <span className="name"></span>
-      <img src={url} alt={name}/>
-      <div className="description" >{description}</div>
-      <div className="ingredients">{ingredients}</div>
-      <div className="time">{time}</div>
-    </div>
-
-    </li>
+    <div>{eachRecipe}</div>
   )
 }
 
