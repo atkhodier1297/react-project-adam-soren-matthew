7009559665a42d3353fe20bafca01341360d5fb5
Adam Khodier <khodieradam1297@gmail.com>
2022-10-25 11:30:22 -0400

    home route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter} from "react-router-dom"
 
-const root = ReactDOM.createRoute(document.getElementById("root"))
-root.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App/>
-        </HashRouter>
-    </React.StrictMode>
-)
+ReactDOM.render(
+<React.StrictMode>
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
+</React.StrictMode>
+
+, document.getElementById('root'));
