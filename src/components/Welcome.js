ecebe3a863aa2aa2248930869a6e353ec52db3ed
Adam Khodier <khodieradam1297@gmail.com>
2022-10-27 16:51:11 -0400

    more css done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -13,10 +13,10 @@ function Welcome() {
         alt=""
       />
       <form action="https://github.com/atkhodier1297">
-        <input type="submit" value="Adam's GitHub" />
+        <input className="adams-git" type="submit" value="Adam's GitHub" />
       </form>
       <form action="https://www.linkedin.com/in/adam-khodier-70336a219/">
-        <input type="submit" value="Adam's LinkedIn" />
+        <input className="adams-linked" type="submit" value="Adam's LinkedIn" />
       </form>
       <img
         src="https://ca.slack-edge.com/T02MD9XTF-U0412QNLKEV-35d483ca213d-512"
@@ -24,10 +24,10 @@ function Welcome() {
         alt=""
       />
       <form action="https://github.com/Sarsur99">
-        <input type="submit" value="Soren's GitHub" />
+        <input className="sorens-git" type="submit" value="Soren's GitHub" />
       </form>
-      <form action="">
-        <input type="submit" value="Soren's LinkedIn" />
+      <form action="https://www.linkedin.com/in/soren-sonstein-3732a4254/">
+        <input className="sorens-linked" type="submit" value="Soren's LinkedIn" />
       </form>
       <img
         src="https://ca.slack-edge.com/T02MD9XTF-U03J6R93QUF-6c16f9a86af2-512"
@@ -35,10 +35,10 @@ function Welcome() {
         alt=""
       />
       <form action="https://github.com/matthew-lui">
-        <input type="submit" value="Matthew's GitHub" />
+        <input className="matts-git" type="submit" value="Matthew's GitHub" />
       </form>
       <form action="https://www.linkedin.com/in/Lui-MatthewM/">
-        <input type="submit" value="Matthew's LinkedIn" />
+        <input className="matts-linked" type="submit" value="Matthew's LinkedIn" />
       </form>
     </div>
   );
